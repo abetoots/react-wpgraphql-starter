d571742814b6ba6e3d1451ce65c368f7033f8d39
Abe Suni M. Caymo <45987443+abetoots@users.noreply.github.com>
2020-03-11 18:22:06 +0800

    need text copyright to pass test

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.scss";
 
 const Footer = props => (
   <footer className="Footer">
-    <div>{new Date().getFullYear()} &copy; All rights reserved.</div>
+    <div>{new Date().getFullYear()} Copyright &copy; All rights reserved.</div>
   </footer>
 );
 
