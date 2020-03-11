8cb1e5918fc77d42e97127c15877f2ee44370c73
Abe Suni M. Caymo <45987443+abetoots@users.noreply.github.com>
2020-03-11 18:20:45 +0800

    add logo, provide menu with linklist

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,9 @@ import Logo from "../UI/Logo/Logo";
 import BurgerMenu from "../UI/BurgerMenu/BurgerMenu";
 import Menu from "../UI/Menu/Menu";
 
+import logo from "../../assets/react-wpgraphql-logo.svg";
+import { getLinkList } from "../../misc/shared/link-list";
+
 const Header = props => {
   const [menuToggled, setMenuToggled] = useState(false);
 
@@ -16,12 +19,12 @@ const Header = props => {
   return (
     <section className="HeaderWrap">
       <header className="Header">
-        <Logo src={} alt="Site Logo" />
+        <Logo src={logo} alt="React WP GraphQl Logo" />
         <BurgerMenu handleClick={menuClickHandler} toggled={menuToggled} />
       </header>
       <div ref={targetElToDisplay} className="HeaderWrap__slot -menu">
         <Menu
-          linklist={}
+          linklist={getLinkList()}
           visible={menuToggled}
           touchTargetStyles={{
             borderBottom: "1px solid #ccc"
