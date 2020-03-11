e5a2e4ebdfa8a8433a66885370aff05dd341f58e
Abe Suni M. Caymo <45987443+abetoots@users.noreply.github.com>
2020-03-11 18:26:10 +0800

    add mutations for authentication

--- src/misc/shared/constants.js
+++ src/misc/shared/constants.js
@@ -1,7 +1,42 @@
-import gql from "graphql-tag";
 //!DO NOT MODIFY
 export const JWT_AUTH_EXPIRATION = "jwtAuthExpiration";
 export const REFRESH_TOKEN = "refreshToken";
 export const AUTH_TOKEN = "authToken";
 
 //Your code below
+/**
+ * Queries
+ */
+export const getLoginMutation = (login, password) => {
+  return `
+    mutation LoginUser{
+      login(
+        input: {
+          clientMutationId: ""
+          username: "${login}"
+          password: "${password}"
+        }
+      ) {
+        ${AUTH_TOKEN}
+        ${REFRESH_TOKEN}
+        user {
+            ${JWT_AUTH_EXPIRATION}
+        }
+      }
+    }
+  `;
+};
+
+export const getRefreshMutation = () => {
+  return `
+    mutation RefreshToken {
+      refreshJwtAuthToken(
+        input: { clientMutationId: "", jwtRefreshToken: "${localStorage.getItem(
+          REFRESH_TOKEN
+        )}" }
+      ) {
+        ${AUTH_TOKEN}
+      }
+    }
+  `;
+};
