6d4103284e089855db99c365a8bfc5a5b32c3037
Abe Suni M. Caymo <45987443+abetoots@users.noreply.github.com>
2020-03-12 20:56:56 +0800

    remove unused import

--- src/misc/shared/helper-funcs.js
+++ src/misc/shared/helper-funcs.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import { createMemoryHistory } from "history";
 import { Router } from "react-router-dom";
-import { REFRESH_TOKEN, JWT_AUTH_EXPIRATION, GYM_ROLE } from "./constants";
+import { REFRESH_TOKEN, JWT_AUTH_EXPIRATION } from "./constants";
 
 /**
  *Ensures that all values from a given array are unique
